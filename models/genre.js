837f7c286dbf948d1c3146699275563a28f661ae
Steven Bruno <sbruno636@gmail.com>
2020-01-19 13:33:39 -0600

    create genre display

--- models/genre.js
+++ models/genre.js
@@ -2,18 +2,18 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var GenreInstanceSchema = new Schema(
+var GenreSchema = new Schema(
   {
-    name: { type: String, required: true, min: 3, max: 100 },
+    name: { type: String, required: true, min: 3, max: 100 }
   }
 );
 
-// Virtual for bookinstance's URL
-GenreInstanceSchema
+// Virtual for Genre's URL
+GenreSchema
   .virtual('url')
   .get(function () {
     return '/catalog/genre/' + this._id;
   });
 
 //Export model
-module.exports = mongoose.model('GenreInstance', GenreInstanceSchema);
+module.exports = mongoose.model('Genre', GenreSchema);
